6e2baa98a9a4f0930dbb50605662c2148fff6596
keeganhauser <keeghauser08@gmail.com>
2023-01-14 21:21:44 -0800

    Added wall class

---
+++ TKGame/Wall.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKGame
+{
+    internal class Wall
+    {
+        public Rectangle Rect { get; private set; }
+        public Texture2D Texture { get; private set; }
+
+        public Wall(int x, int y, int width, int height, GraphicsDevice graphicsDevice)
+        {
+            Rect = new Rectangle(x, y, width, height);
+            Texture = new Texture2D(graphicsDevice, 1, 1);
+            Texture.SetData(new Color[] { Color.White });
+        }
+    }
+}
