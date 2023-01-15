fc035d87dca41724359c45280d13ed2b140dde36
keeganhauser <keeghauser08@gmail.com>
2023-01-14 21:25:12 -0800

    Added additional wall for collision tests

--- TKGame/TKGame.cs
+++ TKGame/TKGame.cs
@@ -3,9 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 
-namespace TKGame 
+namespace TKGame
 {
-    public class TKGame : Game 
+    public class TKGame : Game
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
@@ -14,7 +14,7 @@ namespace TKGame
         List<Wall> walls;
         int screenWidth, screenHeight;
 
-        public TKGame() 
+        public TKGame()
         {
             graphics = new GraphicsDeviceManager(this);
             graphics.PreferredBackBufferWidth = 1600;
@@ -23,7 +23,7 @@ namespace TKGame
             IsMouseVisible = true;
         }
 
-        protected override void Initialize() 
+        protected override void Initialize()
         {
             // TODO: Add your initialization logic here
             screenWidth = graphics.PreferredBackBufferWidth;
@@ -31,25 +31,26 @@ namespace TKGame
 
             // TODO: Remove magic numbers
             // We'll eventually probably want to generate walls based off level data (from a file).
-            walls = new List<Wall>() 
+            walls = new List<Wall>()
             {
-                new Wall(0, screenHeight - 40, screenWidth, 50, graphics.GraphicsDevice),
-                new Wall(0, 0, screenWidth, 50, graphics.GraphicsDevice),
-                new Wall(0, 0, 50, screenHeight - 250, graphics.GraphicsDevice),
-                new Wall(screenWidth - 50, 0, 50, screenHeight - 250, graphics.GraphicsDevice)
+                new Wall(0, screenHeight - 40, screenWidth, 50, graphics.GraphicsDevice),       // Floor 
+                new Wall(0, 0, screenWidth, 50, graphics.GraphicsDevice),                       // Ceiling
+                new Wall(0, 0, 50, screenHeight - 250, graphics.GraphicsDevice),                // Left wall
+                new Wall(screenWidth - 50, 0, 50, screenHeight - 250, graphics.GraphicsDevice), // Right wall
+                new Wall(screenWidth / 2, screenHeight / 2, 250, 250, graphics.GraphicsDevice)
             };
 
             base.Initialize();
         }
 
-        protected override void LoadContent() 
+        protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
 
-        protected override void Update(GameTime gameTime) 
+        protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
@@ -59,7 +60,7 @@ namespace TKGame
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime) 
+        protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.SlateGray);
 
@@ -73,7 +74,7 @@ namespace TKGame
 
             spriteBatch.End();
 
-            
+
             base.Draw(gameTime);
         }
     }
