e7f93f35f758bf9bdafd587d47ea7383ffef65e7
keeganhauser <keeghauser08@gmail.com>
2023-01-14 21:21:12 -0800

    Added walls to the screen

--- TKGame/TKGame.cs
+++ TKGame/TKGame.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace TKGame 
 {
     public class TKGame : Game 
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        // TODO: Refactor out of the main TKGame class
+        List<Wall> walls;
+        int screenWidth, screenHeight;
 
         public TKGame() 
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 900;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -19,13 +26,25 @@ namespace TKGame
         protected override void Initialize() 
         {
             // TODO: Add your initialization logic here
+            screenWidth = graphics.PreferredBackBufferWidth;
+            screenHeight = graphics.PreferredBackBufferHeight;
+
+            // TODO: Remove magic numbers
+            // We'll eventually probably want to generate walls based off level data (from a file).
+            walls = new List<Wall>() 
+            {
+                new Wall(0, screenHeight - 40, screenWidth, 50, graphics.GraphicsDevice),
+                new Wall(0, 0, screenWidth, 50, graphics.GraphicsDevice),
+                new Wall(0, 0, 50, screenHeight - 250, graphics.GraphicsDevice),
+                new Wall(screenWidth - 50, 0, 50, screenHeight - 250, graphics.GraphicsDevice)
+            };
 
             base.Initialize();
         }
 
         protected override void LoadContent() 
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
@@ -45,7 +64,16 @@ namespace TKGame
             GraphicsDevice.Clear(Color.SlateGray);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
 
+            foreach (Wall wall in walls)
+            {
+                spriteBatch.Draw(wall.Texture, wall.Rect, Color.Beige);
+            }
+
+            spriteBatch.End();
+
+            
             base.Draw(gameTime);
         }
     }
