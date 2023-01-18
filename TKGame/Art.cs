996751815233a4626356abeb712d96ce8a904ada
Nate Castle <nate.castle@oit.edu>
2023-01-17 19:14:16 -0800

    Added basic Art.cs for textures and Player.cs functions

---
+++ TKGame/Art.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TKGame
+{
+    static class Art
+    {
+        public static Texture2D Player { get; private set; }
+
+        // "Enemy" will be the name of one such as "Goblin" since it has its own art
+        // public static Texture2D Enemy { get; private set; }
+
+        public static void LoadContent(ContentManager content)
+        {
+            Player = content.Load<Texture2D>("Art/Player"); // Will need to be modified when we create a better structure for our files.
+        }
+    }
+}
--- TKGame/Player.cs
+++ TKGame/Player.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 
 namespace TKGame
 {
-    class Player
+    class Player : Entity
     {
+        private static Player instance;
+        public static Player Instance
+        {
+            get
+            {
+                // Creates the player if it doesn't already exist
+                if (instance == null)
+                    instance = new Player();
 
+                return instance;
+            }
+        }
+
+        private Player()
+        {
+            image = Art.Player;
+        }
+
+        public override void Update()
+        {
+            // Player Movement
+            const float movementSpeed = 10;
+
+        }
     }
 }
