313807a7c96ebfb201bdb9afe15965ad9dc5fd10
aaronw84 <aaronwhitley84@gmail.com>
2023-02-03 12:26:19 -0800

    Created Enemy class and stubbed out with comments

---
+++ TKGame/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace TKGame
+{
+    class Enemy : Entity
+    {
+        private static Enemy instance;
+        private static object syncRoot = new object();
+
+        // TODO: create enemy instance
+
+        // TODO: create enemy constructor
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: draw enemy
+    }
+}
