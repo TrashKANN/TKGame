e5aa3b6282b2087b1c356aab8bb1f43591238891
Nate Castle <nate.castle@oit.edu>
2023-02-05 14:59:46 -0800

    Added to-be-drawn Wall outline while building. Adjusted JSON save location to be relative to working dir

--- TKGame/Level Editor Content/LevelEditor.cs
+++ TKGame/Level Editor Content/LevelEditor.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+// MonoGame includes
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
+// JSON includes
+using System.IO;
+using System.Text.Json;
+
 namespace TKGame.Level_Editor_Content
 {
+    public class WallData
+    {
+        public int X { get; set;}
+        public int Y { get; set;}
+        public int width { get; set;}
+        public int height { get; set;}
+    }
     static class LevelEditor
     {
         //private static GraphicsDevice graphics;
         //private static SpriteBatch spriteBatch;
         private static MouseState previousMouseState;
         private static Vector2 startPosition;
-        private static bool EditMode = false;
+        internal static bool EditMode = false;
 
         public static void ToggleEditor()
         {
             EditMode = !EditMode;
         }
-        public static void BuildWall(Stage stage, GraphicsDevice graphics)
+        public static void BuildWall(Stage stage, GraphicsDevice graphics, SpriteBatch spriteBatch)
         {
             MouseState currentMouseState = Mouse.GetState();
             Vector2 topLeftPosition = new Vector2();
@@ -33,6 +46,19 @@ namespace TKGame.Level_Editor_Content
             {
                 startPosition = new Vector2(currentMouseState.X, currentMouseState.Y);
             }
+            // If the left mouse button IS STILL pressed last update AND IS pressed this update, draw an outline rectangle of the to-be Wall
+            else if (previousMouseState.LeftButton == ButtonState.Pressed &&
+                    currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                Vector2 endPosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+                topLeftPosition.X = Math.Min(startPosition.X, endPosition.X);
+                topLeftPosition.Y = Math.Min(startPosition.Y, endPosition.Y);
+                size = new Vector2(Math.Abs(startPosition.X - endPosition.X), Math.Abs(startPosition.Y - endPosition.Y));
+
+                Rectangle tempRect = new Rectangle((int)topLeftPosition.X, (int)topLeftPosition.Y, (int)size.X, (int)size.Y);
+
+                GameDebug.DrawBoundingBox(spriteBatch, tempRect, Color.Blue, 5);
+            }
             // If the left mouse button IS ALREADY pressed and WAS RELEASED this update, store coordinates for end position
             else if (previousMouseState.LeftButton == ButtonState.Pressed &&
                      currentMouseState.LeftButton == ButtonState.Released)
@@ -41,13 +67,57 @@ namespace TKGame.Level_Editor_Content
                 topLeftPosition.X = Math.Min(startPosition.X, endPosition.X);
                 topLeftPosition.Y = Math.Min(startPosition.Y, endPosition.Y);
                 size = new Vector2(Math.Abs(startPosition.X - endPosition.X), Math.Abs(startPosition.Y - endPosition.Y));
+
+
+                Wall newWall = new Wall(((int)topLeftPosition.X), ((int)topLeftPosition.Y), ((int)size.X), ((int)size.Y), graphics);
+
+                stage.walls.Add(newWall);
             }
 
             previousMouseState = currentMouseState;
+        }
 
-            Wall newWall = new Wall(((int)topLeftPosition.X), ((int)topLeftPosition.Y), ((int)size.X), ((int)size.Y), graphics);
+        public static void SaveStageDataToJSON(Stage stage)
+        {
+            List<WallData> wallDataList = new List<WallData>();
 
-            stage.walls.Add(newWall);
+            // For each wall, Add the data to the WallData list
+            foreach (Wall wall in stage.walls)
+            {
+                if (wall.Rect.Width != 0 && wall.Rect.Height != 0)
+                {
+                    WallData walldata = new WallData()
+                    {
+                        X = wall.Rect.X,
+                        Y = wall.Rect.Y,
+                        width = wall.Rect.Width,
+                        height = wall.Rect.Height,
+                    };
+
+                    wallDataList.Add(walldata);
+                }
+            }
+
+            // Serializes the data set. The Options make the output human-readable.
+            string json = JsonSerializer.Serialize(wallDataList, new JsonSerializerOptions
+            {
+                WriteIndented= true,
+            });
+
+            // Saves the json into the Level Editor Content Folder for now.
+            string directory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../Level Editor Content/"));
+            // = "[Your path into TKGames]\\TKGames\\TKGame\\Level Editor Content\\"
+            // ^ So that we can change it later easily
+
+            // Creates the Directory if it doesn't exist.
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            string path = Path.Combine(directory, "stage_data.json");
+
+            File.WriteAllText(path, json);
         }
     }
 }
