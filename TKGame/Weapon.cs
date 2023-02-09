dbb1845a27c617354ffe9d3b8db5c41fe9fe655d
NathanGr33n <nathan.green.223@gmail.com>
2023-02-09 15:46:28 -0800

    Created Weapon Abstract Class

---
+++ TKGame/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKGame
+{
+    public abstract class Weapon
+    {
+        protected Texture2D weaponTexture;
+        public Vector2 position, velocity;
+        public SpriteEffects orientation;
+
+        public Color color = Color.White;
+        public int damageStat;
+
+        public Vector2 size;
+
+        public abstract int DamageEnemy(Weapon weapon);
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(weaponTexture, position, null, color, 0, size / 2f, 1f, orientation, 0);
+        }
+    }
+}
