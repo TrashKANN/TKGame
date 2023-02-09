5a9f3ce6e27f0c28ea3222e17f01f051d2b98acf
aaronw84 <aaronwhitley84@gmail.com>
2023-02-09 12:42:35 -0800

    Enemy patrols back and forth within boundary

--- TKGame/Enemy.cs
+++ TKGame/Enemy.cs
@@ -14,8 +14,14 @@ namespace TKGame
     {
         private static Enemy instance;
         private static object syncRoot = new object();
-        Vector2 startPosition = new Vector2(500, 500);
+        Vector2 spawn = new Vector2(250, 700);           // to set initial Position
+        Vector2 speed = new Vector2(1, 1);               // to set initial Velocity
+        int rightBoundary = 1000;                        // right boundary variable for Enemy's patrol
+        int leftBoundary = 200;                          // left boundary variable for Enemy's patrol
 
+        /// <summary>
+        /// Create instance of Enemy object with locking for gaurantee
+        /// </summary>
         public static Enemy Instance
         {
             get
@@ -36,23 +42,42 @@ namespace TKGame
 
         /// <summary>
         /// Enemy constructor
+        /// sets texture, initial spawn point, and x-coordinate velocity for Enemy
         /// </summary>
         private Enemy()
         {
             entityTexture = Art.EnemyTexture;
-            Position = startPosition;
+            Position = spawn;
+            Velocity.X = speed.X;
         }
 
         /// <summary>
-        /// Updates Enemy sprite
+        /// Enemy Update method
+        /// calls UpdateLocation() to handle the work for patrolling enemy animation
         /// </summary>
         /// <param name="gameTime"></param>
         /// <exception cref="NotImplementedException"></exception>
         public override void Update(GameTime gameTime)
         {
-            // enemy moves to right
-            Velocity.X = 1f;
-            Position += Velocity;
+            UpdateLocation();
+        }
+
+        /// <summary>
+        /// Updates Enemy location 
+        /// uses formula to make Enemy sprite move bakc and forth within boundaries
+        /// </summary>
+        public void UpdateLocation()
+        {
+            Position.X += Velocity.X;                       // Enemy initially starts moving to right
+
+            if (Position.X >= 1000)
+            {
+                Velocity.X = -speed.X;                      // turn around Enemy to move left
+            }
+            else if (Position.X <= 200)
+            {
+                Velocity.X = speed.X;                       // Enemy goes back to right
+            }
         }
 
         /// <summary>
