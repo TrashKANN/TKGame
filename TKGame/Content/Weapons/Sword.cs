0dc0809ddc9134bf1e14126cc2166c8d2f34e02b
NathanGr33n <nathan.green.223@gmail.com>
2023-03-02 16:44:43 -0800

    Finished Weapon System UI, Still implementing Weapons

---
+++ TKGame/Content/Weapons/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKGame.Content.Weapons
+{
+    class Sword : Weapon
+    {
+        public override int DamageEnemy(Weapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
