17aea6d07d7d83b39ffba92db38423f7366dd95a
aaronw84 <aaronwhitley84@gmail.com>
2023-03-11 12:13:14 -0800

    IEnemies interface, Goblin and Knight enemy type classes

---
+++ TKGame/Goblin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKGame
+{
+    public class Goblin : IEnemies
+    {
+        public string GetEnemyType()
+        {
+            return "Goblin";
+        }
+    }
+}
