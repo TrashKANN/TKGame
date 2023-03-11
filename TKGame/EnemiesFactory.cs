59b04d8cfb2f1817a02c0422fe11e784cb1a8482
aaronw84 <aaronwhitley84@gmail.com>
2023-03-11 12:21:16 -0800

    EnemiesFactory class created for the new IEnemies, Goblin, and Knight. Need to modify client code

---
+++ TKGame/EnemiesFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKGame
+{
+    public class EnemiesFactory
+    {
+        public static IEnemies GetEnemy(string enemyType)
+        {
+            IEnemies enemyDetails = null;
+
+            if (enemyType == "Goblin")
+            {
+                enemyDetails = new Goblin();
+            }
+            else if (enemyType == "Knight")
+            {
+                enemyDetails = new Knight();
+            }
+
+            return enemyDetails;
+        }
+    }
+}
