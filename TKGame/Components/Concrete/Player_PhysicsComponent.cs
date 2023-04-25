994db5af7ec9378df17ee69d6a5e5601fdee5bbc
aaronw84 <aaronwhitley84@gmail.com>
2023-04-25 11:29:31 -0700

    Realized this is stubbed out in Player_InputComponents.cs

--- TKGame/Components/Concrete/Player_PhysicsComponent.cs
+++ TKGame/Components/Concrete/Player_PhysicsComponent.cs
@@ -11,10 +11,9 @@ namespace TKGame.Components.Concrete
     internal class Player_PhysicsComponent : PhysicsComponent
     {
         private static readonly float GRAVITY = 1.0f;
-        ///////////////////////VARIABLES FOR JUMPING LOGIC///////////////////////
+        // variables for jumping logic
         private static readonly float JUMP_IMPULSE = -10.0f;
         private bool isJumping = false;
-        ///////////////////////VARIABLES FOR JUMPING LOGIC///////////////////////
 
         void PhysicsComponent.Update(Entity entity, GameTime gameTime/*, World &world*/) // The &reference isn't working.
         {
@@ -23,28 +22,23 @@ namespace TKGame.Components.Concrete
             Vector2 endVel = entity.Velocity;
             endVel += entity.MOVEMENT_SPEED * entity.Velocity * deltaTime;
 
+            // apply downward gravity
             endVel.Y += GRAVITY;
 
+            // apply jump impulse if jumping
+
+            // update position
             entity.Position += endVel;
 
+            // update hitbox
             entity.HitBox = new Rectangle(((int)entity.Position.X - ((int)entity.Size.X / 2)),
                                             ((int)entity.Position.Y - (int)entity.Size.Y / 2),
                                             (int)entity.Size.X,
                                             (int)entity.Size.Y);
 
+            // clamp position to screen bounds
             //world.resolveCollisions(entity.Position, entity.HitBox);
             entity.Position = Vector2.Clamp(entity.Position, entity.Size / 2, TKGame.ScreenSize - entity.Size / 2);
-
-            ///////////////////////////////////////JUMPING LOGIC///////////////////////////////////////////
-            // apply gravity
-
-            //apply jump impulse if jumping
-
-            // update position and hitbox
-
-            // clamp position to screen bounds
-
-            ///////////////////////////////////////JUMPING LOGIC///////////////////////////////////////////
 
             // The world should own all of the Stage stuff & entities
             // so we would call it here within the PhysicsComponent instead of
