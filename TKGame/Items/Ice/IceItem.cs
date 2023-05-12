56c6eae9b9d6a4d16a6076be73d0f728b2ead7c6
aaronw84 <aaronwhitley84@gmail.com>
2023-05-12 12:24:11 -0700

    Stubbed out classes for different item types

---
+++ TKGame/Items/Ice/IceItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TKGame.BackEnd;
+using TKGame.Components.Concrete;
+using TKGame.Components.Interface;
+using TKGame.Items.Potion.Components;
+using TKGame.PowerUps;
+using TKGame.Status_Effects;
+
+namespace TKGame.Items.Ice
+{
+    public class IceItem : Item
+    {
+        public IceItem() { }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TKGame/Items/Poison/PoisonItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TKGame.BackEnd;
+using TKGame.Components.Concrete;
+using TKGame.Components.Interface;
+using TKGame.Items.Potion.Components;
+using TKGame.PowerUps;
+using TKGame.Status_Effects;
+
+namespace TKGame.Items.Poison
+{
+    public class PoisonItem : Item
+    {
+        public PoisonItem() { }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
