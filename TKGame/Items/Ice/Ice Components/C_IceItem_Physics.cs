119d90b8f432853c6af2cb5441a477d6a99e98ee
aaronw84 <aaronwhitley84@gmail.com>
2023-05-16 16:03:01 -0700

    Ice item physics and graphics components

--- TKGame/Items/Ice/Ice Components/C_IceItem_Physics.cs
+++ TKGame/Items/Ice/Ice Components/C_IceItem_Physics.cs
@@ -5,26 +5,14 @@ using System.Drawing;
 using TKGame.PowerUps.Components;
 using TKGame.Players;
 
-namespace TKGame.Items.FireStone.Components
+namespace TKGame.Items.Ice.Components
 {
-    class C_FireStoneItem_Physics : IPhysicsComponent
+    class C_IceItem_Physics : IPhysicsComponent
     {
         public ComponentType Type => throw new System.NotImplementedException();
 
         public void Update(Entity entity, GameTime gameTime)
         {
-            //Player player = Player.Instance;
-
-            //if (player != null)
-            //{
-            //    Vector2 playerPosition = player.Position;
-
-            //    // if player is within pickup range
-            //    if (player.HitBox.Intersects(entity.HitBox))
-            //    {
-
-            //    }
-            //}
             entity.HitBox = new Microsoft.Xna.Framework.Rectangle((int)entity.Position.X - (int)entity.Size.X / 2,
                                 (int)entity.Position.Y - (int)entity.Size.Y / 2,
                                 (int)entity.Size.X,
--- TKGame/Items/Ice/IceItem.cs
+++ TKGame/Items/Ice/IceItem.cs
@@ -1,24 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TKGame.BackEnd;
+using TKGame.Components.Concrete;
+using TKGame.Components.Interface;
+using TKGame.Items.Ice.Components;
+using TKGame.PowerUps;
+using TKGame.Status_Effects;
 
 // base class for ice item
 namespace TKGame.Items.Ice
 {
     public class IceItem : Item
     {
+        private IPhysicsComponent iceItemPhysics = new C_IceItem_Physics();
+        private IGraphicsComponent iceItemGraphics = new C_IceItem_Graphics();
+        static Random rand = new Random();
+        int x = rand.Next(900, 1200);
+
         public IceItem() 
         {
-            
+            Name = "Ice"; // name for this item is "Ice"
+            Stats = 0;       // stats for this item is 0
+            entityTexture = Art.IceItemTexture;
+            entityType = EntityType.Item;
+            Position = new Vector2(x, 839); // random x-coor spawn
+            HitBox = new Microsoft.Xna.Framework.Rectangle((int)Position.X - (int)(Size.X / 2), (int)Position.Y - (int)(Size.Y / 2), (int)Size.X, (int)Size.Y);
+            components = new Dictionary<ComponentType, List<IComponent>>
+            {
+
+            };
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            iceItemPhysics.Update(this, gameTime/*, world*/);
         }
 
         public void Draw()
         {
-            throw new NotImplementedException();
+            iceItemGraphics.Update(this);
         }
     }
 }
