86eb2569aca71ee44c1d558d65fb71b40e0f1c0f
Keegan Hauser <keeghauser08@gmail.com>
2023-05-19 01:23:01 -0700

    Added powerups to game ui

--- TKGame/UI/GameMenu.cs
+++ TKGame/UI/GameMenu.cs
@@ -8,6 +8,9 @@ using Myra.Graphics2D.UI;
 using Microsoft.Xna.Framework;
 using Myra.Graphics2D.TextureAtlases;
 using TKGame.BackEnd;
+using FontStashSharp;
+using System.IO;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TKGame.UI
 {
@@ -15,13 +18,14 @@ namespace TKGame.UI
     {
         public IMultipleItemsContainer Container { get { return grid; } }
         private Grid grid;
-        private Grid headerGrid;
         private DebugMenu debugMenu;
         private Panel panel;
-        private Image image;
-        private HorizontalStackPanel hsp;
-        private readonly int numCols = 1;
+        private HorizontalStackPanel panelHsp;
+        private FontSystem fontSystem;
+
+		private readonly int numCols = 1;
         private readonly int numRows = 2;
+        private readonly int fontSize = 24;
 
 
         public GameMenu()
@@ -34,12 +38,15 @@ namespace TKGame.UI
         {
             grid = new Grid();
             grid.ShowGridLines = true;
-            headerGrid = new Grid();
             debugMenu = new DebugMenu();
             panel = new Panel();
-            image = new Image();
-            hsp = new HorizontalStackPanel();
-        }
+            panelHsp = new HorizontalStackPanel();
+            panelHsp.ShowGridLines = true;
+
+			byte[] ttfData = File.ReadAllBytes(@"Content/Fonts/Retro Gaming.ttf");
+			fontSystem = new FontSystem();
+			fontSystem.AddFont(ttfData);
+		}
 
         private void LoadContent()
         {
@@ -53,40 +60,74 @@ namespace TKGame.UI
                 grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
             }
 
-            for (int i = 0; i < 3; i++)
-            {
-                headerGrid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-            }
 
-			headerGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
-            headerGrid.GridRow = 0;
-            headerGrid.GridColumn = 0;
-            //grid.Widgets.Add(headerGrid);
-
-			//panel.Background = new SolidBrush(Color.Beige);
+            //panel.Background = new SolidBrush(Color.Beige);
             panel.GridRow = 0;
             panel.GridColumn = 0;
-            panel.Height = 75;
+            panel.Height = 125;
             panel.Width = TKGame.ScreenWidth;
+            panel.Margin = new Myra.Graphics2D.Thickness(100, 50, 0, 0);
             grid.Widgets.Add(panel);
 
-            panel.Widgets.Add(hsp);
+            panelHsp.Spacing = 10;
+            panel.Widgets.Add(panelHsp);
 
-            image.Renderable = new TextureRegion(
-                Art.WeaponTexture, 
-                new Rectangle(
-                    0,
-                    0,
-                    Art.WeaponTexture.Width,
-                    Art.WeaponTexture.Height));
-            image.GridRow = 0;
-            image.GridColumn = 0;
-            image.Width = 75;
-            hsp.Widgets.Add(image);
+            
+            AddWidgetToHeaderPanel(CreateImageWidget(Art.WeaponTexture, 70, 70), "Weapon");
+			AddWidgetToHeaderPanel(CreateImageWidget(Art.FireBallTexture, 60, 35), "Q");
+			AddWidgetToHeaderPanel(CreateImageWidget(Art.SunBurstTexture, 60, 35, new Rectangle(0, 0, 400, 153)), "R");
 
-            (debugMenu.Container as VerticalStackPanel).GridRow = 1;
+
+			(debugMenu.Container as VerticalStackPanel).GridRow = 1;
             (debugMenu.Container as VerticalStackPanel).GridColumn = 0;
             grid.Widgets.Add(debugMenu.Container as VerticalStackPanel);
+
+        }
+
+        private Image CreateImageWidget(Texture2D texture, int width, int height)
+        {
+            return CreateImageWidget(texture, width, height, new Rectangle(0, 0, texture.Width, texture.Height));
+        }
+
+		private Image CreateImageWidget(Texture2D texture, int width, int height, Rectangle textureBounds)
+		{
+			Image image = new Image();
+			image.Renderable = new TextureRegion(texture, textureBounds);
+			image.Width = width;
+			image.Height = height;
+			image.VerticalAlignment = VerticalAlignment.Bottom;
+
+			return image;
+		}
+
+		private void AddWidgetToHeaderPanel(Widget widget, string text = null)
+        {
+            
+            Grid newGrid = new Grid();
+
+            newGrid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+            newGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+            newGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+            newGrid.RowSpacing = 10;
+            newGrid.VerticalAlignment = VerticalAlignment.Bottom;
+
+            widget.GridColumn = 0;
+            widget.GridRow = 0;
+            newGrid.Widgets.Add(widget);
+
+            if (text is not null)
+            {
+                Label widgetLabel = new Label();
+                widgetLabel.Text = text;
+                widgetLabel.Font = fontSystem.GetFont(fontSize);
+                widgetLabel.HorizontalAlignment = HorizontalAlignment.Center;
+                widgetLabel.GridColumn = 0;
+                widgetLabel.GridRow = 1;
+
+                newGrid.Widgets.Add(widgetLabel);
+            }
+
+			panelHsp.Widgets.Add(newGrid);
         }
     }
 }
