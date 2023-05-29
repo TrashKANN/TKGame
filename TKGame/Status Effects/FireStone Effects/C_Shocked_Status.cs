5e93359ad9c668b259bf58d25446103d5af0d4f1
aaronw84 <aaronwhitley84@gmail.com>
2023-05-29 10:33:15 -0700

    C_Shocked_Status stubbed out

--- TKGame/Status Effects/FireStone Effects/C_Shocked_Status.cs
+++ TKGame/Status Effects/FireStone Effects/C_Shocked_Status.cs
@@ -10,7 +10,34 @@ using TKGame.Components.Interface;
 
 namespace TKGame.Status_Effects
 {
-    internal class C_Shocked_Status
+    public class C_Shocked_Status : IStatusComponent
     {
+        public ComponentType Type => ComponentType.Shocked;
+        public StatusType StatusType => StatusType.Shocked;
+        public float Duration { get; set; }
+        public float TickInterval { get; set; }
+        public float DamagePerTick { get; set; }
+        public Entity SourceEntity { get; set; }
+        public float ElapsedTime { get; set; }
+        public float TimeSinceLastTick { get; set; }
+
+        public C_Shocked_Status(float duration, float tickInterval, float damagePerTick, Entity sourceEntity)
+        {
+            Duration = duration;
+            TickInterval = tickInterval;
+            DamagePerTick = damagePerTick;
+            SourceEntity = sourceEntity;
+
+            ElapsedTime = 0f;
+            TimeSinceLastTick = 0f;
+        }
+        public void Update(GameTime gameTime, Entity entity)
+        {
+           throw new NotImplementedException();
+        }
+        public Texture2D GetEffectTexture()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
